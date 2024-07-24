f58416202db7ac674832a8d28db48da9ffccec99
Jayanatha Nanayakkara <112909962+Jayanath-Nanayakkara@users.noreply.github.com>
2024-07-24 22:33:35 +0530

    4 comment jayanath

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "./navbar/Logo";
+import Nav from "./navbar/Nav";
+import { LuMenu } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">NavBar</h1>
+      <nav className="flex p-4 md:p-10 flex-row bg-white sticky top-0 z-20 justify-between items-center">
+        <Logo />
+        <div className="flex space-x-4 md:space-x-10">
+          <Nav />
+        </div>
+        <div className="md:hidden">
+          {menuOpen ? (
+            <RxCross2
+              className="text-gray-600 cursor-pointer w-6 h-6"
+              onClick={toggleMenu}
+            />
+          ) : (
+            <LuMenu
+              className="text-gray-600 cursor-pointer w-6 h-6"
+              onClick={toggleMenu}
+            />
+          )}
+        </div>
+      </nav>
     </div>
   );
 };
---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="md:flex gap-10 hidden ">
+      <NavLink to="/" className="text-gray-600 hover:text-gray-900">
+        Home
+      </NavLink>
+      <NavLink to="/about-us" className="text-gray-600 hover:text-gray-900">
+        About Us
+      </NavLink>
+      <NavLink to="/quotation" className="text-gray-600 hover:text-gray-900">
+        Quotation
+      </NavLink>
+      <NavLink to="/gallery" className="text-gray-600 hover:text-gray-900">
+        Gallery
+      </NavLink>
+      <NavLink to="/contact-us" className="text-gray-600 hover:text-gray-900">
+        Contact Us
+      </NavLink>
+    </div>
+  );
+};
+
+export default Nav;
